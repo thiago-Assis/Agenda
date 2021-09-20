8ea1bda8ac8ca6bf6043b15191cb07b39a64759c
thiago-Assis <65728831+thiago-Assis@users.noreply.github.com>
2021-09-20 15:53:15 -0300

    Add files via upload

---
+++ src/models/HomeModel.js
@@ -0,0 +1,15 @@
+const { String } = require('core-js');
+const mongoose = require('mongoose');
+
+const HomeSchema = new mongoose.Schema({
+    titulo: {type: String, required: true},
+    descricao: String
+});
+
+const HomeModel = mongoose.model('Home', HomeSchema);
+
+class Home {
+    
+}
+
+module.exports = HomeModel;
